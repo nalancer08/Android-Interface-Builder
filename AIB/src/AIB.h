215eb148f405bff99e001ceffc2f128920a68acc
nalancer08 <erick.sanchez@thewebchi.mp>
2016-10-04 13:00:14 -0500

    New methods

--- AIB/src/AIB.h
+++ AIB/src/AIB.h
@@ -15,13 +15,30 @@
 class AIB_ {
 	public:
 		AIB_(int typeInterface, Print &print);
+
 		void setSerialSpeed(int);
 		int establishConnection(void);
+
+		void setAPIDelimiter(char delimiter);
+		char getDelimiter(void);
+
+		void start(void);
+		void startWithInt(int value);
+		void startWithChar(char value);
+		void startWithString(String value);
+
+		void sendInt(int value, bool returnLine = false);
+		void sendChar(char value, bool returnLine = false);
+		void send(String value, bool returnLine = false);
+
 		//bool checkInstance(void);
 		void prettySerial(void);
 	private:
 		int _typeInterface;
 		int _serialSpeed;
+
+		char _delimiter;
+
 		Print* _printer;
 		//bool _completeSetup;
 };
